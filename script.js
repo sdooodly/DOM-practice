0262a18ebed812be1191e6772ac0560e15faf0cd
sdooodly <gayathrigs96@gmail.com>
2023-05-11 22:38:12 +0530

    ?

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ const red = document.getElementById('red');
 const green = document.getElementById('green');
 const blue = document.getElementById('blue');
 
-red.onclick = () => {
-    red.innerText = 0;
-    red.style.backgroundColor = 'pink';
-    console.log('red');
-}
+// red.onclick = () => {
+//     // red.innerText = 0;
+//     // red.style.backgroundColor = 'pink';
+//     console.log('red');
+// }
 
-green.onclick = () => {
-    green.innerText = 0;
-    green.style.backgroundColor = 'yellow';
-    console.log('green');
-}
-blue.onclick = () => {
-    blue.innerText = 0;
-    blue.style.backgroundColor = 'violet';
-    console.log('blue');
-}
+// green.onclick = () => {
+//     green.innerText = 0;
+//     green.style.backgroundColor = 'yellow';
+//     console.log('green');
+// }
+// blue.onclick = () => {
+//     blue.innerText = 0;
+//     blue.style.backgroundColor = 'violet';
+//     console.log('blue');
+// }
 
 const circles = document.querySelectorAll('.square');
 const timesClicked = {'red': 0, 'green': 0, 'blue': 0};
